d98a13faa9c4e126cf83787c3376c5435687a829
Travier Moorlag <moorlagt@gmail.com>
2023-12-04 10:58:17 -0500

    Added migration support

---
+++ src/framework/cmd/createMigration.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+const migrationName = process.argv[3];
+if (!migrationName) {
+  console.error("Please provide a migration name.");
+  process.exit(1);
+}
+
+const now = new Date();
+const date = now.toISOString().split("T")[0].replace(/-/g, "");
+const time = now.toTimeString().split(" ")[0].replace(/:/g, "");
+const filename = `${migrationName}_${date}_${time}.ts`;
+
+const srcPath = path.join("src/database/migrations", filename);
+
+console.log(srcPath);
+
+fs.copyFileSync("src/framework/stubs/migration.stub.ts", srcPath);
+console.log(`Migration created: ${srcPath}`);
