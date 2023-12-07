a08af2c5ae7c4372bc174ff45fb212dc309982ef
Travier Moorlag <moorlagt@gmail.com>
2023-12-07 10:27:28 -0500

    Refactor handleErrors function and createErrorBag utility

--- src/framework/validators/handleErrors.tsx
+++ src/framework/validators/handleErrors.tsx
@@ -1,54 +1,39 @@
-import { Hook } from "@hono/zod-validator";
 import { Context, Env } from "hono";
-import { P } from "pino";
-import { ZodError, ZodSchema, z } from "zod";
+import { ZodError } from "zod";
 import { ErrorBag } from "../globalProps";
-import { ComponentType, VNode } from "preact";
+import { ComponentType } from "preact";
 import { renderComponent } from "../renderer/renderComponent";
 
-export function handleErrors<T, E extends Env, P extends string, O = {}>(
+export function createErrorBag<T>(
   result:
     | { success: true; data: T }
-    | { success: false; error: ZodError; data: T },
-  c: Context<E, P>,
-  fallbackComponent: VNode
+    | { success: false; error: ZodError; data: T }
 ) {
   if (!result.success) {
     let errorBag: ErrorBag = {};
     for (const err of result.error.errors) {
       errorBag[err.path.join(".")] = err.message;
     }
+
+    return errorBag;
   }
+
+  return;
 }
 
-// export function handleErrorBag(
-//   callback: (errorBag: ErrorBag) => Promise<Response>
-// ) {
-//   return handleErrors;
-//   //  return handleErrors((result, c) => {});
-// }
-
-export function handleErrorBag<
-  T,
-  E extends Env,
-  P extends string,
-  O = {}
->(
+export function handleErrorBag<T, E extends Env, P extends string, O = {}>(
   context: Context<E, P>,
   result:
     | { success: true; data: T }
     | { success: false; error: ZodError; data: T },
-  Component: ComponentType<{ errorBag: ErrorBag }>,
+  Component: ComponentType<{ errorBag: ErrorBag }>
 ) {
-  if (!result.success) {
-    let errorBag: ErrorBag = {};
-    for (const err of result.error.errors) {
-      errorBag[err.path.join(".")] = err.message;
-    }
+  const errorBag = createErrorBag(result);
 
+  if (errorBag) {
     return renderComponent(
       context,
-      <Component errorBag={errorBag} {...(context as O)} />
+      <Component errorBag={errorBag} />
     );
   }
 
