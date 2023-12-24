1ff672dc6accd0c76e6cfb8f51f140860d66830e
Travier Moorlag <moorlagt@gmail.com>
2023-12-24 12:50:15 -0500

    Clean up income table

--- src/views/pages/Dashboard/IncomeTable.tsx
+++ src/views/pages/Dashboard/IncomeTable.tsx
@@ -10,60 +10,63 @@ export type Income = {
 export function IncomeTable(props: Props) {
   return (
     <div id="incomeTable" class="relative overflow-x-auto shadow-md sm:rounded">
-      <table class="w-full text-sm text-left text-gray-500">
-        <thead class="text-xs text-gray-700 uppercase bg-green-500">
-          <tr>
-            <th class="pl-2">Income</th>
-            <th>Cost</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {props.income.map((income) => (
+      <form
+        id="incomeTableForm"
+        hx-post="/dashboard/income"
+        hx-target="#incomeTable"
+        hx-swap="outerHTML"
+      >
+        <table class="w-full text-sm text-left text-gray-500">
+          <thead class="text-xs text-gray-700 uppercase bg-green-500">
             <tr>
-              <td class="pl-2">{income.name}</td>
-              <td>{income.amount}</td>
+              <th class="pl-2">Income</th>
+              <th>Cost</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {props.income.map((income) => (
+              <tr>
+                <td class="pl-2">{income.name}</td>
+                <td>{income.amount}</td>
+                <td>
+                  <a
+                    href="#"
+                    class="font-medium text-red-600 dark:text-red-500 hover:underline"
+                  >
+                    Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
+
+            <tr>
+              <td class="whitespace-nowrap">
+                <input
+                  name="name"
+                  type="text"
+                  class="pl-2 form-input shadow-sm mt-1 block w-full"
+                  placeholder="Income"
+                />
+              </td>
+              <td class="whitespace-nowrap">
+                <input
+                  name="amount"
+                  type="text"
+                  class="form-input shadow-sm mt-1 block w-full"
+                  placeholder="Cost"
+                />
+              </td>
               <td>
-                <a
-                  href="#"
-                  class="font-medium text-red-600 dark:text-red-500 hover:underline"
-                >
-                  Delete
-                </a>
+                <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                  Add
+                </button>
               </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div>
-        <form
-          hx-ext="debug"
-          hx-post="/dashboard/income"
-          hx-target="#incomeTable"
-          hx-swap="outerHTML"
-          class="flex flex-row"
-        >
-          <input
-            name="name"
-            type="text"
-            class="pl-2 form-input shadow-sm mt-1 block w-full"
-            placeholder="Income"
-          />
-
-          <input
-            name="amount"
-            type="text"
-            class="form-input shadow-sm mt-1 block w-full"
-            placeholder="Cost"
-          />
-
-          <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-            Add
-          </button>
-        </form>
-      </div>
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 }
