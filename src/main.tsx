89c575ae4d4a6ad827c44e9f8d70caa09b04cab5
Travier Moorlag <moorlagt@gmail.com>
2024-01-31 22:45:18 -0500

    Working on code cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import "reflect-metadata";
 import "./polyfills/compressionStream";
 import { Hono } from "hono";
-import { bootRouter } from "./routers/router";
+import baseRouter from "./routers/router";
 import { showRoutes } from "hono/dev";
 import { setIndexComponent } from "enzo/core";
 import { Index } from ".";
+import { secureHeaders } from "hono/secure-headers";
 
 setIndexComponent((children) => <Index>{children}</Index>);
 
 const app = new Hono();
+app.use("*", secureHeaders());
 
-bootRouter(app);
+app.route("/", baseRouter);
+
 showRoutes(app);
 
 export default app;
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,17 +1,12 @@
-import { Env, Hono } from "hono";
-import { secureHeaders } from "hono/secure-headers";
+import { Hono } from "hono";
 import { baseRouter } from "./baseRouter";
 import { userRouter } from "./userRouter";
 import { dashboardRouter } from "../pages/Dashboard/Dashboard.router";
-import { compress } from "hono/compress";
 
-export function bootRouter(app: Hono<Env, {}, "/">) {
-  app.use("*", secureHeaders());
-  //app.use("*", compress())
-  //app.use("*", logger());
-  //app.use("*", authGuard());
+const app = new Hono();
 
-  app.route("/", baseRouter);
-  app.route("/", userRouter);
-  app.route("/", dashboardRouter);
-}
+app.route("/", baseRouter);
+app.route("/", userRouter);
+app.route("/", dashboardRouter);
+
+export default app;
