cfa2ca1728410a080b0c5ddb5fd24ac2ab78ea93
Travier Moorlag <moorlagt@gmail.com>
2024-02-01 21:31:55 -0500

    Remove example code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
-import "reflect-metadata";
-import "./polyfills/compressionStream";
 import { Hono } from "hono";
-import baseRouter from "./routers/router";
+import router from "./routers";
 import { showRoutes } from "hono/dev";
 import { setIndexComponent } from "enzo/core";
 import { Index } from ".";
@@ -10,9 +8,9 @@ import { secureHeaders } from "hono/secure-headers";
 setIndexComponent((children) => <Index>{children}</Index>);
 
 const app = new Hono();
-app.use("*", secureHeaders());
 
-app.route("/", baseRouter);
+app.use("*", secureHeaders()).use();
+app.route("/", router);
 
 showRoutes(app);
 
---
+++ src/routers/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+
+const app = new Hono();
+
+app.use("/public/*.{js,css}", serveStatic({ root: "./public/" }));
+
+app.get("/ping", async () => {
+  return new Response("pong", { status: 200 });
+});
+
+export default app;
